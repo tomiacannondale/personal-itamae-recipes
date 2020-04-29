6c432ea42bcdfb83b7740327cd8bde6c0f44fe53
tomi <tomiacannondale@gmail.com>
2020-04-29 14:39:59 +0900

    install chromedriver by cask

--- cookbooks/rails_dependencies/default.rb
+++ cookbooks/rails_dependencies/default.rb
@@ -1,9 +1,11 @@
+require 'itamae/plugin/resource/cask'
+
 case node[:platform]
 when 'debian', 'ubuntu', 'mint'
   package "libsqlite3-dev"
   package "libmysqlclient-dev"
   package 'chromium-chromedriver'
 when 'osx', 'darwin'
-  package 'chromedriver'
+  cask 'chromedriver'
 when 'redhat', 'fedora', 'amazon' # redhad includes CentOS
 end
