2f260b9931630847af45d7cb99506b4402128b6b
tomi <tomiacannondale@gmail.com>
2020-04-29 14:40:17 +0900

    Fix ricity

--- cookbooks/ricty/default.rb
+++ cookbooks/ricty/default.rb
@@ -1,8 +1,16 @@
+require 'itamae/plugin/resource/cask'
+
 case node[:platform]
 when 'debian', 'ubuntu', 'mint'
   package 'fonts-ricty-diminished'
 when "osx", "darwin"
-  package 'ricty'
+  execute 'tap homebrew/cask-fonts' do
+    command 'brew tap homebrew/cask-fonts'
+    user node[:current_user]
+    not_if 'brew tap  | grep cask-fonts'
+  end
+
+  cask 'font-ricty-diminished'
 when 'redhat', 'fedora', 'amazon' # redhad includes CentOS
 end
 
