0c769237687fc89b8ea899ee0b47e442fee0e416
tomi <tomiacannondale@gmail.com>
2020-04-29 14:58:12 +0900

    Install mysql instead mysql-client
    mysql2 require mysql on mac

--- cookbooks/mysql/default.rb
+++ cookbooks/mysql/default.rb
@@ -1,7 +1,7 @@
 case node[:platform]
 when 'debian', 'ubuntu', 'mint'
 when 'osx', 'darwin'
-  package 'mysql-client'
+  package 'mysql'
 when 'redhat', 'fedora', 'amazon' # redhad includes CentOS
   package 'mysql-client'
 end
