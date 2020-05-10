534c0ee6836d00e9ef17b6b3eb82e5bffd89abff
tomi <tomiacannondale@gmail.com>
2020-05-10 19:47:59 +0900

    Can input japanese on terminal

--- cookbooks/ime/default.rb
+++ cookbooks/ime/default.rb
@@ -6,6 +6,18 @@ when 'debian', 'ubuntu', 'mint'
 when "osx", "darwin"
   cask 'google-japanese-ime'
   cask 'aquaskk'
+
+  user_aquaskk_dir = "\"#{node[:user_dir]}/Library/Application Support/AquaSKK/\""
+  user_keymap_path = "#{user_aquaskk_dir}keymap.conf"
+
+  execute 'keymap' do
+    not_if "ls #{user_keymap_path}"
+    command <<-COMMAND
+      cp "/Library/Input Methods/AquaSKK.app/Contents/Resources/keymap.conf" #{user_aquaskk_dir}
+      sed -i.back -Ee 's/(SKK_JMODE.+$)/\\1||ctrl::0/' #{user_keymap_path}
+    COMMAND
+  end
+
 when 'redhat', 'fedora', 'amazon' # redhad includes CentOS
 end
 
