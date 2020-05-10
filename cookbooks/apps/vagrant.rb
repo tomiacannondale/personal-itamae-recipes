c0ec8784a2e11eee6658a8c29d92bb96b6a0b79a
tomi <tomiacannondale@gmail.com>
2020-05-10 19:47:30 +0900

    Enable shared folder on vagrant

--- cookbooks/apps/vagrant.rb
+++ cookbooks/apps/vagrant.rb
@@ -6,3 +6,8 @@ when 'osx', 'darwin'
   cask 'vagrant'
 when 'redhat', 'fedora', 'amazon' # redhad includes CentOS
 end
+
+execute 'enable shared folder' do
+  not_if 'vagrant plugin list | grep vagrant-vbguest'
+  command 'vagrant plugin install vagrant-vbguest'
+end
