3f2e301b47b2c3eaf146b2fcc6293585943df224
tomi <tomiacannondale@gmail.com>
2022-11-05 00:07:01 +0900

    Add not_if on install yarn

--- cookbooks/nodejs/install_yarn.rb
+++ cookbooks/nodejs/install_yarn.rb
@@ -1,9 +1,11 @@
 execute 'add apt-key' do
+  not_if "apt-key list | grep yarn"
   user 'root'
   command 'curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | apt-key add -'
 end
 
 execute 'Add repository source' do
+  not_if "ls /etc/apt/sources.list.d/yarn.list"
   user 'root'
   command <<-SHELL
     echo "deb https://dl.yarnpkg.com/debian/ stable main" | tee /etc/apt/sources.list.d/yarn.list
