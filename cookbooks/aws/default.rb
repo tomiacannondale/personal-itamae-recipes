b98bf200412b8d3aa3401375f625fe1a4b0d6b33
tomi <tomiacannondale@gmail.com>
2024-06-27 18:08:47 +0900

    Install ecs-exec

--- cookbooks/aws/default.rb
+++ cookbooks/aws/default.rb
@@ -3,3 +3,13 @@ require 'itamae/plugin/resource/cask'
 package 'awscli'
 package 'session-manager-plugin'
 cask 'aws-vault'
+
+# aws-vaultとfzfを利用して、awsの Elastic Container Serviceのコンテナを対話的に選択アクセスする
+execute 'ecs-exec' do
+  not_if "test -e $HOME/bin/ecs-exec"
+  execute "wget https://github.com/pocari/dotfiles/blob/master/bin/ecs-exec"
+end
+execute 'ecs-active-tasks' do
+  not_if "test -e $HOME/bin/ecs-active-tasks"
+  execute "wget https://github.com/pocari/dotfiles/blob/master/bin/ecs-active-tasks"
+end
